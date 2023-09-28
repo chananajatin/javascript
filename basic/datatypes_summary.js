521cb1a1cf0aefcb0ae4cf87b22d55c51aa6b773
JATIN <76086157+chananajatin@users.noreply.github.com>
2023-09-28 15:25:35 +0000

    memory allocation

--- basic/datatypes_summary.js
+++ basic/datatypes_summary.js
@@ -26,3 +26,29 @@ console.log(typeof myfunction);
  the value they hold at runtime and can change throughout 
  the program as we assign different values to them.*/
 // console.table(heroes);
+
+// *******************************MEMORY*****************************************
+
+// primtive data type uses stack memory   copy
+
+// non-primitive data type uses heap memory  refrence
+
+let myName = "jatin"
+let anotherName = myName
+anotherName = 'manu'
+console.log(anotherName);
+console.log(myName);
+
+let user = 
+{
+    email: "jatin.com",
+    upi: "oop"
+}
+let userTwo = user
+
+userTwo.email = "js.com"
+console.log(user)
+
+
+// non primitive datatypes me refrence se pass hota hai value
+
