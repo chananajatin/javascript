4dcac97e5544daabc01913771b07f08332989a47
JATIN <76086157+chananajatin@users.noreply.github.com>
2023-09-28 12:21:27 +0000

    data

---
+++ basic/datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all code  as newer version of js
+// alert(3+3) we are using node.js not browser
+
+console.log(3+3)
+
+let name = "jatin"  //string
+let age = 18        //number  value
+let isLogedin = false // boolean
+
+// number => 2 to power 53
+// big int  
+// string =>""
+// boolean=> true/false
+// null => standalone value
+// undefined => 
+// symbol => to find uniqueness
+
+
+// object
+console.log(typeof("jatin"));  // string
+console.log(typeof(undefined)); // undefined
+console.log(typeof(null));  // object
---
+++ basic/variables.js
@@ -0,0 +1,15 @@
+const accountId = 14434
+let accountEmail = "jatin.com"
+var accountPassword = "123"
+accountCity = "jaipur"
+
+// accountId = 2
+accountEmail = "js.com"
+accountPassword = "2121"
+accountCity = "banglore"\
+account
+/*
+prefere not to use var 
+beacuse of issue in block scope and fuction scope 
+*/
+console.table([accountId , accountEmail , accountPassword , accountCity])
