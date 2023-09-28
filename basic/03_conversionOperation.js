7990e22c8ab18a1d3d854a5ec04792f104ee1b07
JATIN <76086157+chananajatin@users.noreply.github.com>
2023-09-28 12:38:44 +0000

    CONVERSIONS OF DATA TYPES

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "432as"
+ 
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+//  "33abc" => NaN;
+//  true =>1 false=>0
+ 
+let isLogedin = -22;
+let booleanisLoogedIn = Boolean(isLogedin)
+console.log(booleanisLoogedIn);
+//  1 => true;
+// "" => false'
+// "jatin" => true;
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
