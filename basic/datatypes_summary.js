4aa117e781994e302a7c04451fbfab09f838b376
JATIN <76086157+chananajatin@users.noreply.github.com>
2023-09-28 15:17:05 +0000

    datatype summary

---
+++ basic/datatypes_summary.js
@@ -0,0 +1,28 @@
+//  primtive (call by value)
+// 7 types : string , Number , Boolean , null , undefined , symbol , big int
+
+//  non primitive (reference type)
+// Array , objects , function
+
+const heroes =["dora" , "shaktiman"]
+// array is an object
+
+let myObj = {
+    name: "jatin",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("hello");
+}
+console.log(typeof myfunction);
+
+
+
+
+
+/* JavaScript is a dynamically typed language,
+ which means that data types of variables are determined by
+ the value they hold at runtime and can change throughout 
+ the program as we assign different values to them.*/
+// console.table(heroes);
