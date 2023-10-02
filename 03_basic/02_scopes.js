8356c48b44b12e4d4b7fafec03eef25912b13612
JATIN <76086157+chananajatin@users.noreply.github.com>
2023-10-02 19:02:10 +0000

    scopes global and block

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,25 @@
+if(true)
+{
+    let a = 10
+   const b = 20
+   var c = 30 
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+     const username = "jatin"
+     function two()
+     {
+        const website = "youtube"
+        console.log(username);
+     }
+
+    //  console.log(website);
+     two()
+}
+// andr ka function bhar ke function ko acces kr skta hai but
+// bhar wala andr wale function ke variables ko access ni kr Sakta
+one()
