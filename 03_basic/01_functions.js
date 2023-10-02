e6c4b9a373414f4ae0469b58435f4bfbc118e19d
JATIN <76086157+chananajatin@users.noreply.github.com>
2023-10-02 18:30:02 +0000

    rest operator and object and array passing in function

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -15,6 +15,31 @@ function login_details(username = 'sam ')
       return `${username} just logged in`
 
 }
-
- 
 console.log(login_details())
+
+function calculateCartPrice(...num1)
+{
+    return num1;
+}
+// ... in function is called rest operator to pass multiple value to function
+console.log(calculateCartPrice(200,400,500));  // ye console array dega
+
+const user = 
+{
+    username : "jatin",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+
+}
+handleObject(user)
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(anyArray)
+{
+     return anyArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
